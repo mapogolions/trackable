0a4ae32b58f7785b21c25d85687501e8d8182d09
mapogolions <ivanovpavelalex45@gmail.com>
2022-03-19 15:46:10 +0500

    add tests for market watcher

---
+++ InvertedObserver.Samples/ExchangeMarket/MarketWatcher.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Primitives;
+
+namespace InvertedObserver.Samples.ExchangeMarket
+{
+    public class MarketWatcher : IObserver<CurrencyPair>, IDisposable
+    {
+        private readonly CurrencyPair[] _currencies;
+        private readonly List<(DateTime, string[])> _journal = new();
+        private readonly IDisposable _registration;
+
+        public MarketWatcher(params CurrencyPair[] currencies)
+        {
+            _currencies = currencies;
+            var groupObservable = new GroupObservable<CurrencyPair>(currencies);
+            _registration = ChangeToken.OnChange(groupObservable.GetReloadToken, OnChange);
+            OnChange();
+        }
+
+        public IReadOnlyList<(DateTime, string[])> Journal => _journal;
+
+        public void Dispose() => _registration.Dispose();
+
+        private void OnChange()
+        {
+            var utcNow = DateTime.UtcNow;
+            var info = _currencies.Select(x => $"{x.Name} {x.CurrentPrice}").ToArray();
+            _journal.Add((utcNow, info));
+        }
+
+        public override string ToString()
+        {
+            return _journal
+                .Aggregate(string.Empty,
+                    (acc, x) =>
+                        $"{acc}{x.Item1}{Environment.NewLine}{string.Join(Environment.NewLine, x.Item2)}")
+                .Trim();
+        }
+    }
+}
---
+++ InvertedObserver.Tests/ExchangeMarket/MarketWatchTests.cs
@@ -0,0 +1,73 @@
+using System;
+using InvertedObserver.Samples.ExchangeMarket;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace InvertedObserver.Tests.ExchangeMarket
+{
+    public class MarketWatchTests
+    {
+        [Fact]
+        public void ShouldStopWatchingMarket()
+        {
+            var usdJpy = new CurrencyPair("USD/JPY", 119.21m);
+            var gbpJpy = new CurrencyPair("GBP/USD", 157.13m);
+            var marketWatcher = new MarketWatcher(usdJpy, gbpJpy);
+
+            marketWatcher.Dispose();
+            gbpJpy.CurrentPrice = 154m;
+
+            Assert.Single(marketWatcher.Journal);
+        }
+
+        [Fact]
+        public void ShouldCaptureMultipleChanges()
+        {
+            var usdJpy = new CurrencyPair("USD/JPY", 119.21m);
+            var gbpJpy = new CurrencyPair("GBP/USD", 157.13m);
+            using var marketWatcher = new MarketWatcher(usdJpy, gbpJpy);
+
+            usdJpy.CurrentPrice = 121m;
+            gbpJpy.CurrentPrice = 154m;
+
+            Assert.Equal(3, marketWatcher.Journal.Count);
+        }
+
+        [Fact]
+        public void ShouldJournalPriceChange()
+        {
+            var usdJpy = new CurrencyPair("USD/JPY", 119.21m);
+            var gbpJpy = new CurrencyPair("GBP/USD", 157.13m);
+            using var marketWatcher = new MarketWatcher(usdJpy, gbpJpy);
+
+            usdJpy.CurrentPrice = 121m;
+
+            Assert.Equal(2, marketWatcher.Journal.Count);
+        }
+
+
+        [Fact]
+        public void ShouldDump()
+        {
+            var usdJpy = new CurrencyPair("USD/JPY", 119.21m);
+            var gbpJpy = new CurrencyPair("GBP/USD", 157.13m);
+            using var marketWatcher = new MarketWatcher(usdJpy, gbpJpy);
+
+            var expected = "USD/JPY 119.21"
+                         + Environment.NewLine
+                         + "GBP/USD 157.13";
+
+            Assert.EndsWith(expected, marketWatcher.ToString());
+        }
+
+        [Fact]
+        public void ShouldCaptureTheStartPrice()
+        {
+            var usdJpy = new CurrencyPair("USD/JPY", 119.21m);
+            var gbpJpy = new CurrencyPair("GBP/USD", 157.13m);
+            using var marketWatcher = new MarketWatcher(usdJpy, gbpJpy);
+
+            Assert.Single(marketWatcher.Journal);
+        }
+    }
+}
