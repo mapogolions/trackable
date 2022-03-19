91959272122e7a1a783bd767dc58f8afe3193354
mapogolions <ivanovpavelalex45@gmail.com>
2022-03-19 14:11:39 +0500

    add GroupObservable & add exchange market watcher

---
+++ InvertedObserver/GroupObservable.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using Microsoft.Extensions.Primitives;
+
+namespace InvertedObserver
+{
+    public class GroupObservable<T> where T : IObservable
+    {
+        private readonly T[] _observables;
+
+        public GroupObservable(params T[] observables)
+        {
+            _observables = observables;
+        }
+
+        public IChangeToken GetReloadToken() =>
+            new CompositeChangeToken(_observables.Select(x => x.GetReloadToken()).ToArray());
+    }
+}
